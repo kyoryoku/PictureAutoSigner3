3de88e617fc983fd4dc37c51982bb87758fc157c
kyoryoku <kirino3@yandex.by>
2021-07-28 17:19:56 +0300

    2807_1719

---
+++ src/main/java/ui/mainController.java
@@ -0,0 +1,164 @@
+package ui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXToggleButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import model.Command;
+import model.DataModel;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController {
+
+    @FXML private ResourceBundle resources;
+    @FXML private URL location;
+
+    @FXML private JFXListView<Command> lv;
+    @FXML private JFXButton btnAdd;
+    @FXML private JFXButton btnRemove;
+    @FXML private JFXButton btnProfile;
+    @FXML private JFXButton btnHelp;
+
+    //обработчик нажатия на кнопку Добавить
+    @FXML void handleBtnAdd(ActionEvent event) {
+        DataModel.getDataModel().addCommand(new Command());
+    }
+
+    //обработчик нажатия на кнопку Удалить
+    @FXML void handleBtnRemove(ActionEvent event) {
+        if (DataModel.getDataModel().getChosenCommand() != null ||
+                DataModel.getDataModel().getCommands().size() != 0)
+        {
+            DataModel.getDataModel().removeCommand(
+                    DataModel.getDataModel().getChosenCommand()
+            );
+        }
+    }
+
+    //обработчик нажатия на кнопку Профиль
+    @FXML void handleBtnProfile(ActionEvent event) {
+        WindowsManager.getInstance().openProfileWindow();
+    }
+
+    //обработчик нажатия на кнопку Помощь
+    @FXML void handleBtnHelp(ActionEvent event) {
+        WindowsManager.getInstance().openHelpWindow();
+    }
+
+    //обработчик кликов по ListView
+    @FXML void handleLv(MouseEvent event) {
+        if (event.getClickCount() == 2 && lv.getSelectionModel().getSelectedItem() != null) {
+            WindowsManager.getInstance().openCommandWindow();
+        }
+    }
+
+    @FXML void initialize() {
+        initializeButtons();
+        initializeListView();
+    }
+
+    //настройка кнопок
+    private void initializeButtons(){
+        int iconSize = 20;
+        Color iconColor = Color.web("#EEEEEE");
+        Color iconColorAlert = Color.web("#ff1128");
+
+        //Кнопка Добавить
+        FontIcon addIcon = new FontIcon("anto-plus");
+        addIcon.setIconSize(iconSize);
+        addIcon.setIconColor(iconColor);
+        btnAdd.setGraphic(addIcon);
+
+        //Кнопка Удалить
+        FontIcon removeIcon = new FontIcon("anto-delete");
+        removeIcon.setIconSize(iconSize);
+        removeIcon.setIconColor(iconColorAlert);
+        btnRemove.setGraphic(removeIcon);
+
+        //Кнопка Профиль
+        FontIcon profileIcon = new FontIcon("anto-file-ppt");
+        profileIcon.setIconSize(iconSize);
+        profileIcon.setIconColor(iconColor);
+        btnProfile.setGraphic(profileIcon);
+
+        //Кнопка Помощь
+        FontIcon helpIcon = new FontIcon("anto-info-circle");
+        helpIcon.setIconSize(iconSize);
+        helpIcon.setIconColor(iconColor);
+        btnHelp.setGraphic(helpIcon);
+    }
+
+    //Настройка ListView
+    private void initializeListView(){
+        lv.setItems(DataModel.getDataModel().getCommands());
+
+        //настройка содержимого строк лист вью
+        lv.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Command item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (item == null || empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    //Кнопка переключалка активности команды
+                    JFXToggleButton btnActivate = new JFXToggleButton();
+                    btnActivate.setPadding(new Insets(-5, 0, -5, -5));
+                    btnActivate.selectedProperty().bindBidirectional(item.activeProperty());
+                    btnActivate.setText("");
+                    AnchorPane.setLeftAnchor(btnActivate, 0.);
+                    AnchorPane.setTopAnchor(btnActivate, 0.);
+
+                    //Метка, отображающая название команды
+                    Label lblCmdName = new Label();
+                    lblCmdName.textProperty().bindBidirectional(item.nameProperty());
+                    //lblCmdName.disableProperty().bind(item.activeProperty().not());
+                    AnchorPane.setLeftAnchor(lblCmdName, 50.);
+                    AnchorPane.setTopAnchor(lblCmdName, 11.);
+
+                    //Метка, отображающая назначенную комбинацию
+                    Label lblAssignedKey = new Label();
+                    lblAssignedKey.textProperty().bindBidirectional(item.keyCombinationStringProperty());
+                    //lblAssignedKey.disableProperty().bind(item.activeProperty().not());
+                    AnchorPane.setRightAnchor(lblAssignedKey, 50.);
+                    AnchorPane.setTopAnchor(lblAssignedKey, 11.);
+
+                    //Кнопка назначения комбинации с иконкой
+                    FontIcon iconKeyAssign = new FontIcon();
+                    JFXButton btnKeyAssign = new JFXButton();
+                    iconKeyAssign.setIconLiteral("anto-gold");
+                    iconKeyAssign.setIconSize(20);
+                    iconKeyAssign.setIconColor(Color.web("#EEEEEE"));
+                    btnKeyAssign.setGraphic(iconKeyAssign);
+                    AnchorPane.setRightAnchor(btnKeyAssign, -2.);
+                    AnchorPane.setTopAnchor(btnKeyAssign, 5.);
+                    btnKeyAssign.setOnAction(event -> {
+                        DataModel.getDataModel().setChosenCommand(item);
+                        WindowsManager.getInstance().openAssignKeyWindow();
+                    });
+
+                    AnchorPane root = new AnchorPane(btnActivate, lblCmdName, lblAssignedKey, btnKeyAssign);
+                    setText(null);
+                    setGraphic(root);
+                }
+            }
+        });
+
+        lv.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null){
+                DataModel.getDataModel().setChosenCommand(newSelection);
+            }
+        });
+    }
+}
---
+++ src/main/java/ui/profileController.java
@@ -0,0 +1,28 @@
+package ui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import hotkeys.Key;
+import hotkeys.Keys;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.ResourceBundle;
+
+public class profileController {
+    @FXML private ResourceBundle resources;
+    @FXML private URL location;
+
+    @FXML void initialize() {
+
+    }
+
+
+
+}
